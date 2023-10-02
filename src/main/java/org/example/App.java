17ee77d22a9a9b994613cd13153d10b16679cd5b
Санёчек <saska.jukoff@gmail.com>
2023-10-02 11:24:05 +0200

    master

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import org.example.actionservice.ActionCounter;
+
+public class App {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/org/example/actionservice/ActionCounter.java
@@ -0,0 +1,14 @@
+package org.example.actionservice;
+
+public class ActionCounter {
+
+        public ActionCounter() {
+        }
+
+        public void call(int timestamp) {
+        }
+
+        public int getActions(int timestamp) {
+            return 0;
+        }
+}
